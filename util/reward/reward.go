64b6856055ccae057ecd97e1a0ffebf1670fa0bb
rbajollari <rbajollari@gmail.com>
2022-12-15 14:48:38 -0500

    comment typo

--- util/reward/reward.go
+++ util/reward/reward.go
@@ -8,7 +8,7 @@ import (
 // CalculateRewardFactor returns the reward factor calculated using a logarmithic
 // model based on miss counters. missCount is the current miss count, m is the
 // maximum possible miss counts, and s is the smallest miss count in the period.
-// If the logarimthic functions returns NaN the Reward Factor returned will be 0.
+// If the logarimthic function returns NaN the Reward Factor returned will be 0.
 // rewardFactor = 1 - logₘ₋ₛ₊₁(missCount - s + 1)
 func CalculateRewardFactor(missCount uint64, m uint64, s uint64) string {
 	logBase := m - s + 1
