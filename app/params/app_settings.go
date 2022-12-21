7d54af9e12f3788977c8156983d7fac9b08924e3
Aniket Dixit <dixitaniket199@gmail.com>
2022-12-21 21:19:36 +0530

    feat: move init to address prefix (#63)
    * move init to address prefix
    
    * fix vote test
    
    * add comments

--- app/params/app_settings.go
+++ app/params/app_settings.go
@@ -26,13 +26,9 @@ const (
 // Transactions with gas-price smaller than ProtocolMinGasPrice will fail during DeliverTx.
 var ProtocolMinGasPrice = sdk.NewDecCoinFromDec(BondDenom, sdk.MustNewDecFromStr("0.00"))
 
-func init() {
-	// XXX: If other upstream or external application's depend on any of Ojo's
-	// CLI or command functionality, then this would require us to move the
-	// SetAddressConfig call to somewhere external such as the root command
-	// constructor and anywhere else we contract the app.
+// SetAddressPrefixes call this in init to set sdk config's bech32 address prefix
+func SetAddressPrefixes() {
 	SetAddressConfig()
-
 	if AccountAddressPrefix != Name {
 		log.Fatal("AccountAddresPrefix must equal Name")
 	}
