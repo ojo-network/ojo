5f88227ca7f426a3a0e1185cc8052a143fcb8850
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2022-12-28 17:46:32 -0500

    feat: Enforce voting threshold (#65)
    * Enforce voting threshold
    
    * PR suggestions and slight test refactor
    
    * gofmt

--- util/reward/reward.go
+++ util/reward/reward.go
@@ -10,13 +10,13 @@ import (
 // CalculateRewardFactor returns the reward factor calculated using a logarmithic
 // model based on miss counters. missCount is the current miss count, m is the
 // maximum possible miss counts, and s is the smallest miss count in the period.
-// If the logarimthic function returns NaN the Reward Factor returned will be 0.
+// If the logarimthic function returns NaN or Inf the Reward Factor returned will be 0.
 // rewardFactor = 1 - logₘ₋ₛ₊₁(missCount - s + 1)
 func CalculateRewardFactor(missCount sdk.Dec, m sdk.Dec, s sdk.Dec) string {
 	logBase := m.Sub(s).Add(sdk.NewDec(1))
 	logKey := missCount.Sub(s).Add(sdk.NewDec(1))
 	rewardFactor := 1 - (math.Log(logKey.MustFloat64()) / math.Log(logBase.MustFloat64()))
-	if math.IsNaN(rewardFactor) {
+	if math.IsNaN(rewardFactor) || math.IsInf(rewardFactor, 0) {
 		rewardFactor = 0
 	}
 
