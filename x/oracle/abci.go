a01a3f11146ac73ae19f213829c1cd7f224b6c2d
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2023-04-04 10:56:38 -0400

    Update x/oracle/abci.go
    Co-authored-by: Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>

--- x/oracle/abci.go
+++ x/oracle/abci.go
@@ -113,7 +113,7 @@ func CalcPrices(ctx sdk.Context, params types.Params, k keeper.Keeper) error {
 		}
 	}
 
-	// Get validtors that can earn rewards in this Slash Window
+	// Get validators that can earn rewards in this Slash Window
 	validatorRewardSet := types.ValidatorRewardSet{
 		ValidatorMap: make(map[string]bool),
 	}
