662567bbdc8b59971e33412ac64d781436230e1a
zarazan <zarazan@users.noreply.github.com>
2023-07-14 09:41:45 -0600

    add airdrop CLI tx

--- x/airdrop/client/cli/tx.go
+++ x/airdrop/client/cli/tx.go
@@ -1,1 +1,53 @@
 package cli
+
+import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/ojo-network/ojo/x/airdrop/types"
+	"github.com/spf13/cobra"
+)
+
+func GetTxCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Transaction commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(
+		GetCmdClaimAirdrop(),
+	)
+
+	return cmd
+}
+
+func GetCmdClaimAirdrop() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "claim-airdrop [fromAddress] [toAddress]",
+		Args:  cobra.ExactArgs(2),
+		Short: "Claim airdrop for an address",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgClaimAirdrop(args[0], args[1])
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
