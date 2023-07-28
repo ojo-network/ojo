2e0afed5b6ed7cdf19d018b88ce45ed23b4a5edf
rbajollari <rbajollari@gmail.com>
2023-07-28 15:45:18 +0200

    add 1.0.0 upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -16,6 +16,7 @@ func (app App) RegisterUpgradeHandlers() {
 	}
 
 	app.registerUpgrade0_1_4(upgradeInfo)
+	app.registerUpgrade1_0_0(upgradeInfo)
 }
 
 // performs upgrade from v0.1.3 to v0.1.4
@@ -26,6 +27,16 @@ func (app *App) registerUpgrade0_1_4(_ upgradetypes.Plan) {
 			ctx.Logger().Info("Upgrade handler execution", "name", planName)
 			upgrader := oraclekeeper.NewMigrator(&app.OracleKeeper)
 			upgrader.MigrateValidatorSet(ctx)
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
+}
+
+func (app *App) registerUpgrade1_0_0(_ upgradetypes.Plan) {
+	planName := "v1.0"
+	app.UpgradeKeeper.SetUpgradeHandler(planName,
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			ctx.Logger().Info("Upgrade handler execution", "name", planName)
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		},
 	)
