15de488b1cb502ec54303adb5316a192fb12f099
rbajollari <rbajollari@gmail.com>
2023-08-28 17:29:09 -0400

    move fix to params package

--- app/params/proto.go
+++ app/params/proto.go
@@ -2,10 +2,16 @@ package params
 
 import (
 	"github.com/cosmos/cosmos-sdk/types/module"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/cosmos/cosmos-sdk/types/module/testutil"
 )
 
 // MakeEncodingConfig creates an EncodingConfig for Amino-based tests.
 func MakeEncodingConfig(modules ...module.AppModuleBasic) testutil.TestEncodingConfig {
-	return testutil.MakeTestEncodingConfig(modules...)
+	encCfg := testutil.MakeTestEncodingConfig(modules...)
+
+	// register auth type interfaces
+	authtypes.RegisterInterfaces(encCfg.InterfaceRegistry)
+
+	return encCfg
 }
