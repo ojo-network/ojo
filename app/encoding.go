b8c99d014a0d58cd4b238a09b7e1435781c8c0e2
rbajollari <rbajollari@gmail.com>
2023-08-28 17:02:13 -0400

    register encoding auth types

--- app/encoding.go
+++ app/encoding.go
@@ -2,7 +2,7 @@ package app
 
 import (
 	"github.com/cosmos/cosmos-sdk/types/module/testutil"
-
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/ojo-network/ojo/app/params"
 )
 
@@ -12,5 +12,9 @@ func MakeEncodingConfig() testutil.TestEncodingConfig {
 	encodingConfig := params.MakeEncodingConfig()
 	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	ModuleBasics.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
+	// register auth type interfaces
+	authtypes.RegisterInterfaces(encodingConfig.InterfaceRegistry)
+
 	return encodingConfig
 }
