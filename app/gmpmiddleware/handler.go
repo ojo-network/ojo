591e370ffbe4c8628bcb302349662cf8270903a2
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2023-11-10 08:55:47 -0800

    feat: gmp middleware (#315)

---
+++ app/gmpmiddleware/handler.go
@@ -0,0 +1,106 @@
+package gmpmiddleware
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ojo-network/ojo/x/gmp/types"
+)
+
+type GmpKeeper interface {
+	RelayPrice(
+		goCtx context.Context,
+		msg *types.MsgRelayPrice,
+	) (*types.MsgRelayPriceResponse, error)
+	GetParams(ctx sdk.Context) (params types.Params)
+}
+
+type GmpHandler struct {
+	gmp GmpKeeper
+}
+
+func NewGmpHandler(k GmpKeeper) *GmpHandler {
+	return &GmpHandler{
+		gmp: k,
+	}
+}
+
+// HandleGeneralMessage takes the receiving message from axelar,
+// and sends it along to the GMP module.
+func (h GmpHandler) HandleGeneralMessage(
+	ctx sdk.Context,
+	srcChain,
+	srcAddress string,
+	destAddress string,
+	payload []byte,
+	sender string,
+	channel string,
+) error {
+	ctx.Logger().Info("HandleGeneralMessage called",
+		"srcChain", srcChain,
+		"srcAddress", srcAddress,
+		"destAddress", destAddress,
+		"payload", payload,
+		"module", "x/gmp-middleware",
+	)
+
+	err := verifyParams(h.gmp.GetParams(ctx), sender, channel)
+	if err != nil {
+		return err
+	}
+	denoms, err := parsePayload(payload)
+	if err != nil {
+		return err
+	}
+
+	_, err = h.gmp.RelayPrice(ctx,
+		&types.MsgRelayPrice{
+			Relayer:            srcAddress,
+			DestinationChain:   srcChain,
+			DestinationAddress: destAddress,
+			Denoms:             denoms,
+		},
+	)
+	return err
+}
+
+// HandleGeneralMessage takes the receiving message from axelar,
+// and sends it along to the GMP module.
+func (h GmpHandler) HandleGeneralMessageWithToken(
+	ctx sdk.Context,
+	srcChain,
+	srcAddress string,
+	destAddress string,
+	payload []byte,
+	sender string,
+	channel string,
+	coin sdk.Coin,
+) error {
+	ctx.Logger().Info("HandleGeneralMessageWithToken called",
+		"srcChain", srcChain,
+		"srcAddress", srcAddress,
+		"destAddress", destAddress,
+		"payload", payload,
+		"coin", coin,
+	)
+
+	err := verifyParams(h.gmp.GetParams(ctx), sender, channel)
+	if err != nil {
+		return err
+	}
+	denoms, err := parsePayload(payload)
+	if err != nil {
+		return err
+	}
+
+	_, err = h.gmp.RelayPrice(ctx,
+		&types.MsgRelayPrice{
+			Relayer:            srcAddress,
+			DestinationChain:   srcChain,
+			DestinationAddress: destAddress,
+			Denoms:             denoms,
+			Token:              coin,
+		},
+	)
+	return err
+}
