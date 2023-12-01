b8487358e442590d7a8001e3cef5d04ac33f95b9
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2023-12-01 14:51:09 -0800

    chore: add v0.3.0-rc3 upgrade handler (#327)
    ## Description
    
    Adds the v0.3.0-rc3 upgrade handler
    
    ---
    
    ### Author Checklist
    
    _All items are required. Please add a note to the item if the item is not applicable and
    please add links to any relevant follow up issues._
    
    I have...
    
    - [ ] included the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] added `!` to the type prefix if API or client breaking change
    - [ ] added appropriate labels to the PR
    - [ ] targeted the correct branch
    - [ ] provided a link to the relevant issue or specification
    - [ ] added a changelog entry to `CHANGELOG.md`
    - [ ] included comments for [documenting Go code](https://blog.golang.org/godoc)
    - [ ] updated the relevant documentation or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    
    ### Reviewers Checklist
    
    _All items are required. Please add a note if the item is not applicable and please add
    your handle next to the items reviewed if you only reviewed selected items._
    
    I have...
    
    - [ ] confirmed the correct [type prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json) in the PR title
    - [ ] confirmed all author checklist items have been addressed
    - [ ] reviewed state machine logic
    - [ ] reviewed API design and naming
    - [ ] reviewed documentation is accurate
    - [ ] reviewed tests and test coverage
    - [ ] manually tested (if applicable)

--- app/upgrades.go
+++ app/upgrades.go
@@ -36,6 +36,7 @@ func (app App) RegisterUpgradeHandlers() {
 	app.registerUpgrade0_2_2(upgradeInfo)
 	app.registerUpgrade0_3_0(upgradeInfo)
 	app.registerUpgrade0_3_0Rc2(upgradeInfo)
+	app.registerUpgrade0_3_0Rc3(upgradeInfo)
 }
 
 // performs upgrade from v0.1.3 to v0.1.4
@@ -153,6 +154,16 @@ func (app *App) registerUpgrade0_3_0(upgradeInfo upgradetypes.Plan) {
 
 func (app *App) registerUpgrade0_3_0Rc2(_ upgradetypes.Plan) {
 	const planName = "v0.3.0-rc2"
+	app.UpgradeKeeper.SetUpgradeHandler(planName,
+		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			ctx.Logger().Info("Upgrade handler execution", "name", planName)
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
+}
+
+func (app *App) registerUpgrade0_3_0Rc3(_ upgradetypes.Plan) {
+	const planName = "v0.3.0-rc3"
 	app.UpgradeKeeper.SetUpgradeHandler(planName,
 		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			ctx.Logger().Info("Upgrade handler execution", "name", planName)
