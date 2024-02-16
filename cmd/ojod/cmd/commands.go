6b7297a88ce81858182157455213a83a06c79bc6
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2024-02-16 14:48:40 -0500

    Update cmd/ojod/cmd/commands.go
    Co-authored-by: Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>

--- cmd/ojod/cmd/commands.go
+++ cmd/ojod/cmd/commands.go
@@ -37,10 +37,6 @@ import (
 func initCometBFTConfig() *cmtcfg.Config {
 	cfg := cmtcfg.DefaultConfig()
 
-	// these values put a higher strain on node memory
-	// cfg.P2P.MaxNumInboundPeers = 100
-	// cfg.P2P.MaxNumOutboundPeers = 40
-
 	return cfg
 }
 
