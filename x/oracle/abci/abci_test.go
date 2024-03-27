d09cbd6d28f2dd91011d4223a872b725dc5b9b16
rbajollari <rbajollari@gmail.com>
2024-03-26 20:55:58 -0400

    add vote extention and preblocker integration tests

--- x/oracle/abci/abci_test.go
+++ x/oracle/abci/abci_test.go
@@ -1,22 +1,21 @@
 package abci_test
 
 import (
-	// "context"
 	"testing"
-	// "time"
+	"time"
 
 	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ojo-network/ojo/tests/integration"
-	// "github.com/rs/zerolog"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/suite"
 
 	ojoapp "github.com/ojo-network/ojo/app"
-	// "github.com/ojo-network/ojo/pricefeeder"
-	// "github.com/ojo-network/price-feeder/oracle"
-	// "github.com/ojo-network/price-feeder/oracle/client"
-	// "github.com/ojo-network/price-feeder/oracle/provider"
-	// "github.com/ojo-network/price-feeder/oracle/types"
+	"github.com/ojo-network/ojo/pricefeeder"
+	"github.com/ojo-network/price-feeder/oracle"
+	"github.com/ojo-network/price-feeder/oracle/client"
+	"github.com/ojo-network/price-feeder/oracle/provider"
+	"github.com/ojo-network/price-feeder/oracle/types"
 )
 
 type IntegrationTestSuite struct {
@@ -36,48 +35,22 @@ func TestAbciTestSuite(t *testing.T) {
 	suite.Run(t, new(IntegrationTestSuite))
 }
 
-// func MockPriceFeeder(ctx context.Context) *pricefeeder.PriceFeeder {
-// 	providers := make(map[types.ProviderName][]types.CurrencyPair)
-// 	deviations := make(map[string]math.LegacyDec)
-// 	providerEndpointsMap := make(map[types.ProviderName]provider.Endpoint)
+func MockPriceFeeder() *pricefeeder.PriceFeeder {
+	providers := make(map[types.ProviderName][]types.CurrencyPair)
+	deviations := make(map[string]math.LegacyDec)
+	providerEndpointsMap := make(map[types.ProviderName]provider.Endpoint)
 
-// 	oracle := oracle.New(
-// 		zerolog.Nop(),
-// 		client.OracleClient{},
-// 		providers,
-// 		time.Second*10,
-// 		deviations,
-// 		providerEndpointsMap,
-// 		false,
-// 	)
+	oracle := oracle.New(
+		zerolog.Nop(),
+		client.OracleClient{},
+		providers,
+		time.Second*10,
+		deviations,
+		providerEndpointsMap,
+		false,
+	)
 
-// 	oracle.SetPrices(ctx)
-
-// 	return &pricefeeder.PriceFeeder{
-// 		Oracle: oracle,
-// 	}
-// }
-
-// type MockProvider struct {}
-
-// func (mp *MockProvider) GetTickerPrices(pairs ...types.CurrencyPair) (types.CurrencyPairTickers, error) {
-// 	tickerPrices := make(types.CurrencyPairTickers, len(pairs))
-
-// }
-
-// func (mp *MockProvider) GetCandlePrices(pairs ...types.CurrencyPair) (types.CurrencyPairCandles, error) {
-
-// }
-
-// func NewMockProvider() MockProvider {
-// 	ojoPair := types.CurrencyPair{
-// 		Base: "OJO",
-// 		Quote: "USD",
-// 		Address: "OJOADDRESS",
-// 	}
-// 	usdcPair := types.CurrencyPair{
-// 		Base: "USDC",
-// 		Quote: "USD",
-// 		Address: "USDCADDRESS",
-// 	}
-// }
+	return &pricefeeder.PriceFeeder{
+		Oracle: oracle,
+	}
+}
