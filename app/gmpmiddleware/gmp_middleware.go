cfce2e5821ace00cf5deec5f2645cf1ce3c1bfc8
rbajollari <rbajollari@gmail.com>
2024-04-21 15:59:54 -0500

    fix: Use token from ibc packet in GMP handler for all GMP messages

--- app/gmpmiddleware/gmp_middleware.go
+++ app/gmpmiddleware/gmp_middleware.go
@@ -142,47 +142,36 @@ func (im IBCMiddleware) OnRecvPacket(
 		return ack
 	}
 
-	switch msg.Type {
-	case gmptypes.TypeGeneralMessage:
-		err = im.handler.HandleGeneralMessage(
-			ctx,
-			msg.SourceChain,   // e.g. "Ethereum"
-			msg.SourceAddress, // e.g. "0x1234..."
-			data.Receiver,     // e.g. "ojo1..."
-			msg.Payload,
-			data.Sender, // e.g. "ojo1..."
-			packet.DestinationChannel,
-		)
-	case gmptypes.TypeGeneralMessageWithToken:
-		// parse the transfer amount
-		amt, ok := sdk.NewIntFromString(data.Amount)
-		if !ok {
-			return channeltypes.NewErrorAcknowledgement(
-				errors.Wrapf(
-					types.ErrInvalidAmount,
-					"unable to parse transfer amount (%s) into sdk.Int",
-					data.Amount,
-				),
-			)
-		}
-		denom := parseDenom(packet, data.Denom)
-
-		err = im.handler.HandleGeneralMessageWithToken(
-			ctx,
-			msg.SourceChain,
-			msg.SourceAddress,
-			data.Receiver,
-			msg.Payload,
-			data.Sender,
-			packet.DestinationChannel,
-			sdk.NewCoin(denom, amt),
-		)
-	default:
+	if msg.Type != gmptypes.TypeGeneralMessage {
 		ctx.Logger().With(
 			fmt.Errorf("unrecognized message type: %d", msg.Type)).
 			Error("unrecognized gmp message")
 		return ack
 	}
+
+	// parse the transfer amount
+	amt, ok := sdk.NewIntFromString(data.Amount)
+	if !ok {
+		return channeltypes.NewErrorAcknowledgement(
+			errors.Wrapf(
+				types.ErrInvalidAmount,
+				"unable to parse transfer amount (%s) into sdk.Int",
+				data.Amount,
+			),
+		)
+	}
+	denom := parseDenom(packet, data.Denom)
+
+	err = im.handler.HandleGeneralMessage(
+		ctx,
+		msg.SourceChain,   // e.g. "Ethereum"
+		msg.SourceAddress, // e.g. "0x1234..."
+		data.Receiver,     // e.g. "ojo1..."
+		msg.Payload,
+		data.Sender, // e.g. "ojo1..."
+		packet.DestinationChannel,
+		sdk.NewCoin(denom, amt),
+	)
 
 	if err != nil {
 		return channeltypes.NewErrorAcknowledgement(err)
