1ff08a0dec0757b411c4bd5e9a653c5715842674
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2024-04-29 14:52:22 -0400

    chore: v0.3.1-rc2 upgrade handler (#430)

--- app/upgrades.go
+++ app/upgrades.go
@@ -39,6 +39,7 @@ func (app App) RegisterUpgradeHandlers() {
 	app.registerUpgrade0_3_0(upgradeInfo)
 	app.registerUpgrade0_3_0Rc8(upgradeInfo)
 	app.registerUpgrade0_3_1Rc1(upgradeInfo)
+	app.registerUpgrade0_3_1Rc2(upgradeInfo)
 }
 
 // performs upgrade from v0.1.3 to v0.1.4
@@ -178,6 +179,17 @@ func (app *App) registerUpgrade0_3_0Rc8(_ upgradetypes.Plan) {
 
 func (app *App) registerUpgrade0_3_1Rc1(_ upgradetypes.Plan) {
 	const planName = "v0.3.1-rc1"
+	app.UpgradeKeeper.SetUpgradeHandler(planName,
+		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			sdkCtx := sdk.UnwrapSDKContext(ctx)
+			sdkCtx.Logger().Info("Upgrade handler execution", "name", planName)
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
+}
+
+func (app *App) registerUpgrade0_3_1Rc2(_ upgradetypes.Plan) {
+	const planName = "v0.3.1-rc2"
 	app.UpgradeKeeper.SetUpgradeHandler(planName,
 		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			sdkCtx := sdk.UnwrapSDKContext(ctx)
