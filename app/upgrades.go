faac05508f4f9aa340310f7f5aab38597f7d0ae6
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2024-07-26 05:22:49 -0400

    chore: v0.3.2 upgrade handler (#481)
    * chore: v0.3.2 upgrade handler
    
    * convert proposals
    
    * lint
    
    * migrate proposal test

--- app/upgrades.go
+++ app/upgrades.go
@@ -16,7 +16,6 @@ import (
 	consensustypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
 	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
-	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	govv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
@@ -45,6 +44,7 @@ func (app App) RegisterUpgradeHandlers() {
 	app.registerUpgrade0_3_1Rc1(upgradeInfo)
 	app.registerUpgrade0_3_1Rc2(upgradeInfo)
 	app.registerUpgrade0_3_1(upgradeInfo)
+	app.registerUpgrade0_3_2(upgradeInfo)
 	app.registerUpgrade0_4_0(upgradeInfo)
 }
 
@@ -211,12 +211,23 @@ func (app *App) registerUpgrade0_3_1(_ upgradetypes.Plan) {
 	app.UpgradeKeeper.SetUpgradeHandler(planName,
 		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			sdkCtx := sdk.UnwrapSDKContext(ctx)
+			sdkCtx.Logger().Info("Upgrade handler execution", "name", planName)
+			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		},
+	)
+}
+
+func (app *App) registerUpgrade0_3_2(_ upgradetypes.Plan) {
+	const planName = "v0.3.2"
+
+	app.UpgradeKeeper.SetUpgradeHandler(planName,
+		func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			sdkCtx := sdk.UnwrapSDKContext(ctx)
 
 			// migrate old proposals
-			govMigrator := govkeeper.NewMigrator(&app.GovKeeper, app.GetSubspace(govtypes.ModuleName))
-			err := govMigrator.Migrate2to3(sdkCtx)
+			err := oraclekeeper.MigrateProposals(sdkCtx, app.keys[govtypes.StoreKey], app.appCodec)
 			if err != nil {
-				panic("failed to migrate governance module")
+				sdkCtx.Logger().Error("failed to migrate governance proposals", "err", err)
 			}
 
 			sdkCtx.Logger().Info("Upgrade handler execution", "name", planName)
