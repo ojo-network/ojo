c1cb0661e943d5ab2131ee6110da91b250f6d7f2
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2024-09-27 10:57:16 -0700

    enable price feeder appconfig setting in e2e tests

--- tests/e2e/orchestrator/orchestrator.go
+++ tests/e2e/orchestrator/orchestrator.go
@@ -335,6 +335,7 @@ func (o *Orchestrator) runValidators(t *testing.T) {
 			"start",
 			"--pricefeeder.config_path=/root/pricefeeder/price-feeder.example.toml",
 			"--pricefeeder.log_level=info",
+			"--pricefeeder.enabled=true",
 		}
 
 		runOpts := &dockertest.RunOptions{
