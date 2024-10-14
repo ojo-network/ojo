9d7c205c9325e30c3cb40cdec84ae2cc3a9e4137
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2024-10-14 14:00:12 -0700

    easier to read

--- x/gmp/types/payment.go
+++ x/gmp/types/payment.go
@@ -5,16 +5,13 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// TriggerUpdate checks if the payment should be updated based on the current rate and the last update block.
 func (p Payment) TriggerUpdate(rate math.LegacyDec, ctx sdk.Context) bool {
-	// Calculate the percentage difference
-	priceDiff := p.LastPrice.Sub(rate).Abs()
-	percentageDiff := math.LegacyZeroDec()
-	if !p.LastPrice.IsZero() {
-		percentageDiff = priceDiff.Quo(p.LastPrice).MulInt64(100)
-	} else {
-		percentageDiff = math.LegacyNewDec(100)
+	if p.LastBlock == 0 || p.LastPrice.IsZero() {
+		return true
 	}
+	priceDiff := p.LastPrice.Sub(rate).Abs()
+	deviationExceeded := priceDiff.Quo(p.LastPrice).MulInt64(100).GT(p.Deviation)
 
-	return percentageDiff.GT(p.Deviation) ||
-		p.LastBlock < ctx.BlockHeight()-p.Heartbeat
+	return deviationExceeded || p.LastBlock < ctx.BlockHeight()-p.Heartbeat
 }
