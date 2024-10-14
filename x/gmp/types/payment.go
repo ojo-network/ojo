e44530bd9f2408943168aec74d3451657c5afc2a
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2024-10-14 13:44:21 -0700

    fix: handle no previous payment updates

--- x/gmp/types/payment.go
+++ x/gmp/types/payment.go
@@ -8,7 +8,12 @@ import (
 func (p Payment) TriggerUpdate(rate math.LegacyDec, ctx sdk.Context) bool {
 	// Calculate the percentage difference
 	priceDiff := p.LastPrice.Sub(rate).Abs()
-	percentageDiff := priceDiff.Quo(p.LastPrice).MulInt64(100)
+	percentageDiff := math.LegacyZeroDec()
+	if !p.LastPrice.IsZero() {
+		percentageDiff = priceDiff.Quo(p.LastPrice).MulInt64(100)
+	} else {
+		percentageDiff = math.LegacyNewDec(100)
+	}
 
 	return percentageDiff.GT(p.Deviation) ||
 		p.LastBlock < ctx.BlockHeight()-p.Heartbeat
