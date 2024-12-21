060bb34ddc9417ae43496615d94bb50ab70bd2c8
rbajollari <rbajollari@gmail.com>
2024-12-20 19:35:28 -0500

    fmt

--- x/symbiotic/keeper/network_state_change_test.go
+++ x/symbiotic/keeper/network_state_change_test.go
@@ -3,11 +3,19 @@ package keeper_test
 import (
 	"time"
 
+	"cosmossdk.io/math"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
+	stakingtestutil "github.com/cosmos/cosmos-sdk/x/staking/testutil"
+	appparams "github.com/ojo-network/ojo/app/params"
 	"github.com/ojo-network/ojo/x/symbiotic/types"
 )
 
 const (
 	ojoMiddlewareAddress = "0x127E303D6604C48f3bA0010EbEa57e09324A4dF6"
+	secret               = "fixed_seed_value"
 )
 
 func (s *IntegrationTestSuite) TestSymbioticUpdateValidatorsPower() {
@@ -15,6 +23,26 @@ func (s *IntegrationTestSuite) TestSymbioticUpdateValidatorsPower() {
 	ctx = ctx.
 		WithBlockHeight(10).
 		WithBlockTime(time.Now())
+
+	sh := stakingtestutil.NewHelper(s.T(), ctx, app.StakingKeeper)
+	sh.Denom = appparams.BondDenom
+	privKey := secp256k1.GenPrivKeyFromSecret([]byte(secret))
+	pubKey := privKey.PubKey()
+
+	initTokens := sdk.TokensFromConsensusPower(1000, sdk.DefaultPowerReduction)
+	initCoins := sdk.NewCoins(sdk.NewCoin(appparams.BondDenom, initTokens))
+
+	s.app.BankKeeper.MintCoins(s.ctx, minttypes.ModuleName, initCoins)
+	s.app.BankKeeper.SendCoinsFromModuleToAccount(s.ctx, minttypes.ModuleName, sdk.AccAddress(pubKey.Address()), initCoins)
+
+	sh.CreateValidatorWithValPower(
+		sdk.ValAddress(pubKey.Address()),
+		pubKey,
+		1000,
+		true,
+	)
+	val, err := app.StakingKeeper.GetValidator(ctx, sdk.ValAddress(pubKey.Address()))
+	s.Require().Equal(val.GetTokens(), math.NewInt(1000000000))
 
 	params := app.SymbioticKeeper.GetParams(ctx)
 	params.MiddlewareAddress = ojoMiddlewareAddress
@@ -28,10 +56,14 @@ func (s *IntegrationTestSuite) TestSymbioticUpdateValidatorsPower() {
 		Height:    ctx.BlockHeight(),
 	}
 
-	s.app.SymbioticKeeper.SetCachedBlockHash(ctx, cachedBlockHash)
+	app.SymbioticKeeper.SetCachedBlockHash(ctx, cachedBlockHash)
 
-	err = s.app.SymbioticKeeper.SymbioticUpdateValidatorsPower(ctx)
+	err = app.SymbioticKeeper.SymbioticUpdateValidatorsPower(ctx)
 	s.Require().NoError(err)
+
+	val, err = app.StakingKeeper.GetValidator(ctx, sdk.ValAddress(pubKey.Address()))
+	s.Require().NoError(err)
+	s.Require().Equal(val.GetTokens(), math.NewInt(0))
 }
 
 func (s *IntegrationTestSuite) TestFinalizedBlockHash() {
