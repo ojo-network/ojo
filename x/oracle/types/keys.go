4609d25458e86f66c672201eface8f11a4f367ab
rbajollari <rbajollari@gmail.com>
2025-01-09 14:26:32 -0500

    store key name

--- x/oracle/types/keys.go
+++ x/oracle/types/keys.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	// ModuleName is the name of the oracle module
-	ModuleName = "oracle"
+	ModuleName = "ojooracle"
 
 	// StoreKey is the string store representation
 	StoreKey = ModuleName
